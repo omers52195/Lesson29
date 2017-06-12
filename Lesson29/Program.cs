0951485433cb5d052c73399ce1fa7457def1b5ff
unknown <Omer>
2017-06-12 06:31:07 -0700

    Lesson29 Finished

--- Lesson29/Program.cs
+++ Lesson29/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Linq;
 
-namespace OverloadingGenericsLinqAlgorithms
+namespace _Lesson29
 {
     class Overloading
     {
@@ -10,20 +10,48 @@ namespace OverloadingGenericsLinqAlgorithms
         // int and displays that parameter in the console output.
         // The second overloaded version takes a single parameter of type
         // string and displays that parameter in the console output.
-
+        public void Display(int a)
+        {
+            Console.WriteLine(a);
+        }
+        public void Display(string a)
+        {
+            Console.WriteLine(a);
+        }
     }
 
-    class IndexerProperty
-    {
-        // Implement a read-only indexer property that simply
-        // returns the string representation of its int type index value.
+    // class IndexerProperty
+    //{
+    // Implement a read-only indexer property that simply
+    // returns the string representation of its int type index value.
+    // public string Current { get; set; }
+    //public int this[int index]// indexer declaration //public = propertytype(access modifier)
+    //{
+    //  get
+    //{
+    //  return Current[index] ;
+    //}
+    //}
+    //}
 
-    }
+    //  MyProperty = 
 
+    //    }
     class Generics
     {
         // Implement a generic method named GenericSwapMethod that takes
         // two parameters of any type and swaps them in a persistent way.
+        public void GenericSwapMethod<T>(ref T a, ref T b)
+        {
+            T c = a;
+            a = b;
+            b = c;
+        }
+
+        public void Test(ref int x)
+        {
+            x = 5;
+        }
 
     }
 
@@ -36,7 +64,12 @@ namespace OverloadingGenericsLinqAlgorithms
             // in the int array parameter named numbers, and then display
             // the results of the that query in the console output from
             // within a foreach loop.
+            var list = numbers.Where(m => m % 2 == 0).ToList();
 
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 
@@ -46,7 +79,7 @@ namespace OverloadingGenericsLinqAlgorithms
         {
             // Google for a description of "Gauss sum of integers"
             // Then implement that algorithm here and return the result.
-
+            return (limit * (limit + 1)) / 2;
         }
     }
 
@@ -57,7 +90,9 @@ namespace OverloadingGenericsLinqAlgorithms
             // Call the two overloaded versions of the Display method.
             // The first call passes in the int value of 42.
             // The second call passes in the string value of "Hello Overloading".
-
+            var overloading = new Overloading();
+            overloading.Display(42);
+            overloading.Display("Hello Overloading");
 
             // Access the indexer property on the IndexerProperty class,
             // specifying the index value of 42, and display the result
@@ -69,17 +104,27 @@ namespace OverloadingGenericsLinqAlgorithms
             // Then call the GenericSwapMethod, passing in the two parameters.
             // After that, display the values on the console output and see
             // that they are swapped.
+            var x = 3;
+            var y = 4;
+            Console.WriteLine($"x: {x}, y : {y}");
 
+            var generics = new Generics();
+            generics.GenericSwapMethod(ref x, ref y);
+            Console.WriteLine($"x: {x}, y : {y}");
 
             // Declare an array of int named numbers and initialize
             // it with the element values: 1, 2, 3, 4, 5, 6, 7, 8.
             // Then call the DisplayEvens method and see that only even
             // elements are displayed.
-
+            Linq.DisplayEvens(new int[] { 1, 2, 3, 4, 5, 6, 7, 8 });
 
             // Call the AddIntegersFromOneUpTo method with a parameter set to 100.
             // Then display the result on the console output.
+            var z = Gauss.AddIntegersFromOneUpTo(100);
+            Console.WriteLine(z);
 
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadLine();
         }
     }
 }
